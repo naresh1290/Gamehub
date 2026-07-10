f4763617ec3ffd01d600ba9f22d6ae9ccc8abafb
Naresh <vishnatkk@gmail.com>
2026-07-11 01:16:24 +0530

    Slightly reduce mobile game poster (5:4, max 440px centered)

--- wp-content/themes/gamehub/functions.php
+++ wp-content/themes/gamehub/functions.php
@@ -12,7 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'GAMEHUB_THEME_VERSION', '1.3.7' );
+define( 'GAMEHUB_THEME_VERSION', '1.3.8' );
 
 /**
  * True when the GameHub Engine plugin is active and exposing its API.
